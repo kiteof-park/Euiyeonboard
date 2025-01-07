3d05cde863e56e8cc99062ad0e7adcfedf92a5df
kiteof_park <kiteof_park@naver.com>
2025-01-07 20:35:49 +0900

    first commit test

---
+++ src/main/java/com/example/euiyeonboard/EuiyeonboardApplication.java
@@ -0,0 +1,11 @@
+package com.example.euiyeonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuiyeonboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EuiyeonboardApplication.class, args);
+    }
+}
