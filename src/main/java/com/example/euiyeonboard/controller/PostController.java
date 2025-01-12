e6a4370786f5a7e998409b9ffa037122a16a4b74
kiteof_park <kiteof_park@naver.com>
2025-01-12 19:08:07 +0900

    feat: Level2 게시글 전체 조회, 특정 게시글 조회 기능 구현

--- src/main/java/com/example/euiyeonboard/controller/PostController.java
+++ src/main/java/com/example/euiyeonboard/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.euiyeonboard.controller;
 
+import com.example.euiyeonboard.domain.Post;
 import com.example.euiyeonboard.dto.PostRequest;
 import com.example.euiyeonboard.dto.PostResponse;
 import com.example.euiyeonboard.service.PostService;
@@ -7,15 +8,31 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class PostController {
     private final PostService postService;
 
+    // 게시글 작성
     @PostMapping("/post")
     // @ResponseBody - > @RestController로 교체
     public PostResponse savePost(@RequestBody @Valid PostRequest postRequest){
         return postService.savePost(postRequest);
     }
+
+    // 게시글 전체 조회
+    @GetMapping("/post")
+    public List<PostResponse> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    // 특정 게시글 조회
+    @GetMapping("/post/{postId}")
+    public PostResponse getPost(Long id){
+        return postService.getPost(id);
+    }
+
 }
--- src/main/java/com/example/euiyeonboard/service/PostService.java
+++ src/main/java/com/example/euiyeonboard/service/PostService.java
@@ -7,11 +7,16 @@ import com.example.euiyeonboard.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor        // 필드 주입X -> 생성자 주입(@Autowired) -> @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
 
+    // 게시글 작성
     public PostResponse savePost(PostRequest postRequest){
         Post post = Post.builder()
                 .title(postRequest.title())
@@ -26,4 +31,33 @@ public class PostService {
                 .content(savedPost.getContent())
                 .build();
     }
+
+    // 게시글 전체 조회
+    public List<PostResponse> getAllPosts(){
+        List<Post> posts = postRepository.findAll();
+
+        // ToRefactor : stream() 사용해서 코드 리팩토링 가능하지 않나?
+        List<PostResponse> postResponses =  new ArrayList<>();
+
+        for(Post post : posts){
+            postResponses.add(PostResponse.builder()
+                    .id(post.getId())
+                    .title(post.getTitle())
+                    .content(post.getContent())
+                    .build());
+        }
+        return postResponses;
+    }
+
+    // 특정 게시글 조회
+    public PostResponse getPost(Long id){
+        // ToRefactor : Optional 메서드 사용해보기, get() 사용 지양하기!
+        Optional<Post> post = postRepository.findById(id);
+        return PostResponse.builder()
+                .id(post.get().getId())
+                .title(post.get().getTitle())
+                .content(post.get().getContent())
+                .build();
+    }
+
 }
