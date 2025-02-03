4baa66fb8f44ee0e6e12a2bb32e0d98e23113845
kiteof_park <kiteof_park@naver.com>
2025-02-03 21:13:42 +0900

    feat: ResponseEntity<T> 적용

--- src/main/java/com/example/euiyeonboard/controller/PostController.java
+++ src/main/java/com/example/euiyeonboard/controller/PostController.java
@@ -21,37 +21,33 @@ public class PostController {
     // 게시글 작성
     @PostMapping("/post")
     // @ResponseBody - > @RestController로 교체
-    public PostResponse savePost(@RequestBody @Valid PostCreateRequest postCreateRequest){
-
+        // @RestController = @Controller + @ResponseBody
+    public ResponseEntity<PostResponse> savePost(@RequestBody @Valid PostCreateRequest postCreateRequest){
         return postService.savePost(postCreateRequest);
     }
 
     // 게시글 전체 조회
     @GetMapping("/post")
-    public List<PostResponse> getAllPosts(){
-
+    public ResponseEntity<List<PostResponse>> getAllPosts(){
         return postService.getAllPosts();
     }
 
     // 특정 게시글 조회
     @GetMapping("/post/{postId}")
-    public PostResponse getPost(@PathVariable("postId") Long postId){
-
+    public ResponseEntity<PostResponse> getPost(@PathVariable("postId") Long postId){
         return postService.getPost(postId);
     }
 
     // 게시글 수정
     @PatchMapping("/post/{postId}")
-    public PostResponse updatePost(@PathVariable("postId") Long postId,
-                                   @RequestBody @Valid PostUpdateRequest postUpdateRequest){
-
+    public ResponseEntity<PostResponse> updatePost(@PathVariable("postId") Long postId,
+                                                   @RequestBody @Valid PostUpdateRequest postUpdateRequest){
         return postService.updatePost(postId, postUpdateRequest);
     }
 
     // 게시글 삭제
     @DeleteMapping("/post/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable("postId") Long postId){
-        postService.deletePost(postId);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return postService.deletePost(postId);
     }
 }
