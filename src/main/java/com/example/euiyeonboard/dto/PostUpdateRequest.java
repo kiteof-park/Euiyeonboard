59feac4e56c3c9c79fa9434f5d561074808e62bf
kiteof_park <kiteof_park@naver.com>
2025-02-17 18:11:18 +0900

    feat: PostCreateRequest, PostUpdateRequest DTO 유효성 검사 기능 구현

--- src/main/java/com/example/euiyeonboard/dto/PostUpdateRequest.java
+++ src/main/java/com/example/euiyeonboard/dto/PostUpdateRequest.java
@@ -6,10 +6,11 @@ import jakarta.validation.constraints.Size;
 public record PostUpdateRequest(
 
         @NotBlank(message = "제목은 필수 입력값입니다.")
-        @Size(min = 2, max = 30, message = "제목은 2글자 이상 30글자 이하로 작성해야 합니다.")
+        @Size(min = 1, max = 15, message = "제목은 1글자 이상 15글자 이하로 작성해주세요.")
         String title,
 
         @NotBlank(message = "내용은 필수 입력값입니다.")
+        @Size(min = 1, max = 1000, message = "내용은 1글자 이상 1000글자 이하로 작성해주세요.")
         String content
 ) {
 }
