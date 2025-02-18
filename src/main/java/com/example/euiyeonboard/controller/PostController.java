16c27f934748ca2e038ec8539158dfa5d4faba89
kiteof_park <kiteof_park@naver.com>
2025-02-18 16:50:22 +0900

    style: 컨트롤러 url 정리

--- src/main/java/com/example/euiyeonboard/controller/PostController.java
+++ src/main/java/com/example/euiyeonboard/controller/PostController.java
@@ -20,22 +20,23 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/boards")
+@RequestMapping("/boards/posts")
 @Validated      // 검색 키워드 유효성 검사를 위해 추가
 public class PostController {
 
     private final PostService postService;
 
     // 게시글 작성
-    @PostMapping("/posts")
-    // @ResponseBody - > @RestController로 교체
+    @PostMapping
+    // @ResponseBody
+        // @RestController로 교체
         // @RestController = @Controller + @ResponseBody
     public ResponseEntity<PostResponse> savePost(@RequestBody @Valid PostCreateRequest postCreateRequest){
         return postService.savePost(postCreateRequest);
     }
 
     // 게시글 전체 조회 - 페이지네이션⭕, 정렬⭕
-    @GetMapping("/posts1")
+    @GetMapping
     public PagedModel<PostResponse> getAllPosts(@RequestParam(name = "page", defaultValue = "1") int page,
                                                 @RequestParam(name = "size", defaultValue = "15") int size) {
         if(size > 100){ size = 100; }
@@ -44,15 +45,15 @@ public class PostController {
     }
 
     // 게시글 전체 조회 - 페이지네이션❌, 정렬⭕
-    @GetMapping("/posts2")
-    public ResponseEntity<List<PostResponse>> getAllPosts(){
-        return postService.getAllPosts();
-    }
+//    @GetMapping("/posts2")
+//    public ResponseEntity<List<PostResponse>> getAllPosts(){
+//        return postService.getAllPosts();
+//    }
 
 
     // 게시글 검색 - 검색키워드가 포함된 게시글 검색
     // board/post/search?keyword="의연 최고"
-    @GetMapping("/post/search")
+    @GetMapping("/search")
     public PagedModel<PostResponse> getPostByKeyword(@RequestParam @NotBlank(message = "검색 키워드는 공백 제외 1글자 이상 입력해주세요.") String keyword,
                                                      @RequestParam(name = "page", defaultValue = "1") int page,
                                                      @RequestParam(name = "size", defaultValue = "15") int size){
@@ -63,7 +64,7 @@ public class PostController {
 
     // 게시글 검색 - 검색키워드가 포함된 제목의 게시글 검색
     // board/post/search/title?keyword="의연 최고"
-    @GetMapping("/post/search/title")
+    @GetMapping("/search/title")
     public PagedModel<PostResponse> getPostByTitle(@RequestParam @NotBlank(message = "검색 키워드는 공백 제외 1글자 이상 입력해주세요.") String keyword,
                                                    @RequestParam(name = "page", defaultValue = "1") int page,
                                                    @RequestParam(name = "size", defaultValue = "15") int size){
@@ -73,21 +74,21 @@ public class PostController {
     }
 
     // 특정 게시글 조회 - id로 단건 조회
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/{postId}")
     public ResponseEntity<PostResponse> getPost(@PathVariable("postId") Long postId){
         return postService.getPost(postId);
     }
     
 
     // 게시글 수정
-    @PatchMapping("/posts/{postId}")
+    @PatchMapping("/{postId}")
     public ResponseEntity<PostResponse> updatePost(@PathVariable("postId") Long postId,
                                                    @RequestBody @Valid PostUpdateRequest postUpdateRequest){
         return postService.updatePost(postId, postUpdateRequest);
     }
 
     // 게시글 삭제
-    @DeleteMapping("/posts/{postId}")
+    @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable("postId") Long postId){
         return postService.deletePost(postId);
     }
